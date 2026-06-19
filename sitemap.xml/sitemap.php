58c4ad1cbf0f46133912fbca1f468b806008fe28
Keamo <kk2mogoba@gmail.com>
2026-06-19 22:34:39 +0200

    creating sitemap

---
+++ sitemap.xml/sitemap.php
@@ -0,0 +1,28 @@
+
+<?xml version="1.0" encoding="UTF-8"?>
+
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+
+    <url>
+        <loc>Home.html</loc>
+    </url>
+
+    <url>
+        <loc>About.html</loc>
+    </url>
+
+    <url>
+        <loc>services.html</loc>
+    </url>
+
+
+    <url>
+        <loc>contact.html</loc>
+    </url>
+
+    <url>
+        <loc>events.html</loc>
+    </url>
+
+</urlset>
+
